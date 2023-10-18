a44d8732f6aa5bf3b4490a12d2414882ef666b5e
Velosofy <61261357+Velosofy@users.noreply.github.com>
2023-10-19 01:28:44 +0700

    fixes: primarily the buttons to snap correctly

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`)
     })
     .catch(err => console.error(err));
 
-fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc`)
+fetch(`https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc&vote_count.gte=500&with_original_language=en&api_key=${api_key}`)
     .then(response => response.json())
     .then(data => {
         const tvsDiv = $('.tv-collection');
@@ -66,14 +66,9 @@ fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${api_key}&include_adult
 
 
 $(document).ready(function () {
-    $("#movie-carousel .carousel-control-next").on("click", function () {
-        movieScrollPosition += movieSlideWidth;
-        movieCarouselContainer.stop().animate({ scrollLeft: movieScrollPosition }, 1000);
-        if (movieScrollPosition >= movieCarouselContainer[0].scrollWidth - movieCarouselContainer.width()) {
-            $(this).hide();
-        }
-        $("#movie-carousel .carousel-control-prev").show();
-    });
+    if ($(window).width() >= 768) {
+        $("#movie-carousel, #tv-carousel").removeClass("slide");
+    }
 
     $("#movie-carousel .carousel-control-prev").on("click", function () {
         movieScrollPosition -= movieSlideWidth;
@@ -83,4 +78,44 @@ $(document).ready(function () {
         }
         $("#movie-carousel .carousel-control-next").show();
     });
+    $("#movie-carousel .carousel-control-next").on("click", function () {
+        movieScrollPosition += movieSlideWidth;
+        movieCarouselContainer.stop().animate({ scrollLeft: movieScrollPosition }, 1000);
+        if (movieScrollPosition >= movieCarouselContainer[0].scrollWidth - movieCarouselContainer.width()) {
+            $(this).hide();
+        }
+        $("#movie-carousel .carousel-control-prev").show();
+    });
+
+    $("#tv-carousel .carousel-control-prev").on("click", function () {
+        tvScrollPosition -= tvSlideWidth;
+        tvCarouselContainer.stop().animate({ scrollLeft: tvScrollPosition }, 1000);
+        if (tvScrollPosition <= 0) {
+            $(this).hide();
+        }
+        $("#tv-carousel .carousel-control-next").show();
+    });
+
+    $("#tv-carousel .carousel-control-next").on("click", function () {
+        tvScrollPosition += tvSlideWidth;
+        tvCarouselContainer.stop().animate({ scrollLeft: tvScrollPosition }, 1000);
+        if (tvScrollPosition >= tvCarouselContainer[0].scrollWidth - tvCarouselContainer.width()) {
+            $(this).hide();
+        }
+        $("#tv-carousel .carousel-control-prev").show();
+    });
+});
+
+$(window).on('resize', function () {
+    if ($(window).width() >= 768) {
+        $("#movie-carousel, #tv-carousel").removeClass("slide");
+
+        movieSlideWidth = movieCarouselContainer.find(".carousel-item").first().outerWidth(true);
+        movieScrollPosition = Math.round(movieCarouselContainer.scrollLeft() / movieSlideWidth) * movieSlideWidth;
+        movieCarouselContainer.stop().animate({ scrollLeft: movieScrollPosition }, 1000);
+
+        tvSlideWidth = tvCarouselContainer.find(".carousel-item").first().outerWidth(true);
+        tvScrollPosition = Math.round(tvCarouselContainer.scrollLeft() / tvSlideWidth) * tvSlideWidth;
+        tvCarouselContainer.stop().animate({ scrollLeft: tvScrollPosition }, 1000);
+    }
 });
