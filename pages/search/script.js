fd071f65f0af6a0560940d6f4af0e8a947261797
Velosofy <61261357+Velosofy@users.noreply.github.com>
2023-10-20 12:18:35 +0700

    search: add movie/tv select

--- pages/search/script.js
+++ pages/search/script.js
@@ -14,29 +14,75 @@ async function getMovieDetails(movie_id) {
     return data;
 }
 
+async function getTVData(query) {
+    const url = `https://api.themoviedb.org/3/search/tv?api_key=${api_key}&query=${query}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+async function getTVDetails(tv_id) {
+    const url = `https://api.themoviedb.org/3/tv/${tv_id}?api_key=${api_key}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+
 $(() => {
-    $('.search-input').on('keydown', async (event) => {
-        if (event.key === 'Enter') {
-            $('.search-btn').trigger('click');
-        }
-    });
+    var search_type = 'none';
 
-    $('.search-btn').on('click', async () => {
-        const result_container = $('.search-result');
-        result_container.empty();
+    {
+        $('.search-type').on('change', () => {
+            search_type = $('.search-type').val();
+            if (search_type === 'none') {
+                $('.search-btn').attr('disabled', true);
+                $('.search-input').attr('disabled', true);
+                return;
+            }
+            $('.search-input').attr('disabled', false);
+            $('.search-btn').attr('disabled', false);
 
-        const row = $('<div class="row"></div>');
-        result_container.append(row);
-
-        const query = $('.search-input').val();
-        const data = await getMovieData(query);
-        console.log(data);
-
-        data.results.forEach(movie => {
-            const col = $('<div class="col-sm-4 col-md-3 col-lg-2 my-2"></div>');
-            const card = createCard(movie);
-            col.append(card);
-            row.append(col);
+            const input = $('.search-input');
+            input.attr('placeholder', `Search ${search_type}...`);
         });
-    });
+    }
+
+    {
+        $('.search-input').on('keydown', async (event) => {
+            if (event.key === 'Enter') {
+                $('.search-btn').trigger('click');
+            }
+        });
+
+        $('.search-btn').on('click', async () => {
+            console.log(search_type);
+            const result_container = $('.search-result');
+            result_container.empty();
+
+            const row = $('<div class="row"></div>');
+            result_container.append(row);
+
+            const query = $('.search-input').val();
+
+            var data;
+            switch (search_type) {
+                case 'movie':
+                    data = await getMovieData(query);
+                    break;
+                case 'tv':
+                    data = await getTVData(query);
+                    break;
+                default:
+                    return;
+            }
+
+            data.results.forEach(result => {
+                const col = $('<div class="col-sm-4 col-md-3 col-lg-2 my-2"></div>');
+                const card = createCard(result);
+                col.append(card);
+                row.append(col);
+            });
+        });
+    }
 });
